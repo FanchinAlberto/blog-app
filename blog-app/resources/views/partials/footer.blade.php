2e1efb9d122a7622593dae66efb08a5423dc5cbf
FanchinAlberto <83753418+FanchinAlberto@users.noreply.github.com>
2023-03-02 11:27:23 +0100

    Delete blog-app directory

--- blog-app/resources/views/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="blog-footer">
-  <p>Blog template built for <a href="https://www.itisrossi.edu.it/">ITIS Rossi Vicenza</a></p>
-  <p>
-    <a href="#">Back to top</a>
-  </p>
-</footer>
